f2c574bbd9c071ac6c34be207ed4193924754012
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-10 21:21:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,9 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |n|
-    cheese_types.include?(n)
-    return n
+    if cheese_types.include?(n)
+      return n
+    end 
   
   end 
   
