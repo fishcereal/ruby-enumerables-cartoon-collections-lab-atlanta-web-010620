311c9e8e9ba69039c7e78d2d66c70e4336c87e8f
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-10 20:45:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,11 +3,8 @@ def roll_call_dwarves(array)
   array.each_with_index{|item,index|
   hash[item] = index+1
   } 
-  
-  
-  
-  
-  
+  p "hash is "
+  pp hash 
 end
 
 def summon_captain_planet# code an argument here
