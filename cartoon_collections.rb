eadf224f6074213993192f4e8f6edbcdb6eac805
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-10 21:20:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,10 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |n|
     array.include?(cheese_types)
     return n
   end 
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
